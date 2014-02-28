deca90dbd3c194e209cfcdf19bf549c34cb420da
jheg <emailheg@gmail.com>
2014-02-28 20:45:51 +0000

    updating html & css
    unfinished though

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
 <html lang="en">
 <head>
+  <link href='http://fonts.googleapis.com/css?family=Pathway+Gothic+One' rel='stylesheet' type='text/css'>	
   <meta charset="utf-8">
 
   <title>GC Energy Ltd | The #1 Energy Recruiters</title>
   <meta name="description" content="">
   <meta name="author" content="Digimoose">
 
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="style.css">
 
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -17,7 +18,6 @@
 
 <body>
 	<header>
-		<img src="img/gcenergy_logo.png">
 		<nav>
 			<ul>
 				<li><a href="#">Home</a></li>
@@ -27,7 +27,11 @@
 				<li><a href="#">Jobs</a></li>
 			</ul>
 		</nav>
+		<img src="img/gcenergy_logo.png">
+		
 	</header>
+	<section>Rotating banner will go here</section>
+	<main>Main content will go here</main>
 	<section id="send_cv">
 		<h2>Send us your CV</h2>
 		<p>Some text that appeals to potential candidates and encourages them to send you there cv.</p>
@@ -82,6 +86,41 @@
 			</a>
 		</div>
 	</section>
-	<footer>Footer Area!</footer>
+	<footer>
+		<div class="container">
+			<section class="testimonials"><h2>RECENT TESTIMONIALS</h2></section>
+			<section class="address"><h2>OUR ADDRESS</h2></section>
+			<section class="sectors"><h2>BROWSE JOBS BY SECTOR</h2></section>
+			<section>
+			<h2>GET IN TOUCH</h2>
+			<p>Fill out the form and submit your message</p>
+			<form action="/my-handling-form-page" method="post">
+				<div>
+					<label for="first_name"></label>
+					<input type="text" id="first_name" placeholder="first name" />
+				</div>
+				<div>
+					<label for="last_name"></label>
+					<input type="text" id="last_name" placeholder="last name" />
+				</div>
+				<div>
+					<label for="email"></label>
+					<input type="email" id="email" placeholder="email" />
+				</div>
+				<div>
+					<label for="tel"></label>
+					<input type="tel" id="tel" placeholder="tel" />
+				</div>
+				<div>
+					<label for="msg"></label>
+					<textarea id="msg" placeholder="OPTIONAL: enter a message for example: I'm interested in job ID00500"></textarea>
+				</div>
+				<div>
+					<input id="submit" value="Send" type="submit">
+				</div>		
+	    	</form>
+			</section>
+		</div>
+	</footer>
 </body>
 </html>
