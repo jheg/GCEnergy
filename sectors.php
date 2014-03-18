f9a080ae4ec927c09492a4e3bf087a7828b2305d
jheg <jason@jheg.co.uk>
2014-03-18 22:43:20 +0000

    starting to style

---
+++ sectors.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+<h1>Sectors</h1>
+
+<h2>Oil &amp; Gas</h2>
+<ul>
+	<li>Well Services</li>
+	<li>Drilling</li>
+	<li>Subsea Technical</li>
+	<li>Subsea Engineering</li>
+	<li>Pipeline Engineer</li>
+	<li>Structural Engineer</li>
+</ul>
+<h2>Renewable Energy</h2>
+<ul>
+	<li>Wind</li>
+	<li>Mechanical Design Engineer</li>
+	<li>Rope Access</li>
+	<li>Wind Turbine Tech</li>
+</ul>
+<h2>Mining</h2>
+<ul>
+	<li></li>
+</ul>
+<h2>Finance</h2>
+
+Need to understand Sectors, Sub_Sectors &amp; Jobs.
+
+
+</body>
+</html>
