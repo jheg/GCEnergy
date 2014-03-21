987eda29d2ef8d81ef0299fdc92aa30a5b900e70
MikeChipshop <mike@miniman-webdesign.co.uk>
2014-03-21 11:08:03 +0000

    Intro box

--- index.php
+++ index.php
@@ -54,7 +54,6 @@
 	</section>
 	<div class="container"> 
 		<main class="grid_8 omega">
-			Main content will go here <br />Possible tabbed sectors with jobs like on http://www.energyrecruitmentcompany.com/<br />Some intro text about GC Energy
 			<section class="intro">
 				<h1>Headline/strapline to go here</h1>
 				<p>About us text. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
